20ea914896bf32fb3e6fdeb708f89b1ce05e4443
summer0404 <157554348+summer0404@users.noreply.github.com>
2024-12-01 14:30:35 +0700

    change backgound color'

--- src/pages/add_movies/index.jsx
+++ src/pages/add_movies/index.jsx
@@ -19,7 +19,7 @@ const theme = createTheme({
 
 export default function AddMovies() {
   return (
-    <div className="movie-detail-page flex justify-center relative pt-6 bg-purple-800 text-white">
+    <div className="movie-detail-page flex justify-center relative pt-6 bg-gradient-to-r from-[#0B0C10] to-[#1F2833]  text-white">
       <ThemeProvider theme={theme}>
         <MovieForm sx={{with: "90%"}}/>
       </ThemeProvider>
