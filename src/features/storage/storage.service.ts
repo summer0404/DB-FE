d23e6d9a467889f8e5e9e90dbea3bf56e9a15ea9
LinhDoHoang <linhak17cqt@gmail.com>
2024-12-01 10:05:33 +0700

    upload file init

--- src/features/storage/storage.service.ts
+++ src/features/storage/storage.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { initializeApp } from 'firebase/app';
+import { Express } from 'express';
 
 @Injectable()
-export class StorageService {}
+export class StorageService {
+  constructor() {
+    const firebaseConfig = {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTHDOMAIN,
+      projectId: process.env.PROJECTID,
+      storageBucket: process.env.STORAGEBUCKET,
+      messagingSenderId: process.env.MESSAGINGSENDERID,
+      appId: process.env.APPID,
+      measurementId: process.env.MEASUREMENTID,
+    };
+    initializeApp(firebaseConfig);
+  }
+
+  async upLoadFile(file: Express.Multer.File) {
+    // const pages = await this.getPages(file);
+    // const storage = getStorage();
+    // const storageRef = ref(storage);
+    // const metadata = {
+    //   contentType: file.mimetype, // Đảm bảo lưu đúng loại file
+    // };
+    // const key = `${Date.now()}-${file.originalname}`;
+    // const name = file.originalname;
+    // const snapshot = await uploadBytes(
+    //   ref(storageRef, key),
+    //   file.buffer,
+    //   metadata,
+    // );
+    // const path = await getDownloadURL(snapshot.ref);
+    // const size = snapshot.metadata.size;
+    // return {
+    //   size: await this.formatSize(size),
+    //   path,
+    //   key,
+    //   name,
+    //   pages,
+    // };
+  }
+}
